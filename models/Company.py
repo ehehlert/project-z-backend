4bbddf00e181dfb8e73ad7723cf0e9c6609fc941
eric <ehehlert1994@gmail.com>
2025-08-08 09:54:12 -0700

    added User and Company

---
+++ models/Company.py
@@ -0,0 +1,49 @@
+import uuid
+from datetime import datetime, timezone
+from sqlalchemy.dialects.postgresql import UUID, JSONB
+from .db import db
+
+class Company(db.Model):
+    __tablename__ = 'companies'
+    id = db.Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        nullable=False
+    )
+    name = db.Column(
+        db.String,
+        nullable=False
+    )
+    created_at = db.Column(
+        db.DateTime(timezone=True),
+        nullable=False,
+        default=lambda: datetime.now(timezone.utc)
+    )
+    modified_at =db.Column(
+        db.DateTime(timezone=True),
+        onupdate=lambda: datetime.now(timezone.utc)
+    )
+    is_deleted = db.Column(
+        db.Boolean,
+        nullable=False,
+        default=False
+    )
+    active = db.Column(
+        db.Boolean,
+        nullable=False,
+        default=True
+    )
+
+    def to_dict(self):
+        def format_dt(dt):
+            return dt.astimezone(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ') if dt else None
+        
+        return {
+            'id': str(self.id),
+            'name': self.name,
+            'created_at': format_dt(self.created_at),
+            'modified_at': format_dt(self.modified_at),
+            'is_deleted': self.is_deleted,
+            'active': self.active
+        }
